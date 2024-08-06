7be0de0c094a2cdd30eb69171a94bfb62f2b6de8
Aiden Ryan <aiden.ryan@entegris.com>
2024-08-06 16:38:52 -0700

    implemented node and path classes

---
+++ thermal_notebook.py
@@ -0,0 +1,31 @@
+# %%
+import sys
+sys.path.append('/Users/125715/python')
+import math as m
+import numpy as np
+
+from fluidlib import thermal_solver as ts
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+# %%
+T = ts.node(198, 8000, .05, 500)
+T2 = ts.node(400, 8000, .05, 500)
+p = ts.path(1, h=15)
+args = p.getArgs() + T.getArgs()
+T_array = [T.getT(), T2.getT()]
+T_array = [195, 400]
+t = [1, 10000]
+T = solve_ivp(ts.ODE, t, T_array, args=args)
+print(T.y)
+plt.plot(T.t, T.y[1,:])
+
+
+
+
+
+
+
+# %%
--- thermal_solver.py
+++ thermal_solver.py
@@ -4,7 +4,46 @@ ignore radiation
 constant heat transfer coefficient
 
 '''
-def transientODE(t, T, h, A_surface, rho, V, c, q, E_g, Tinf):
-    return q*A_surface + E_g - (h*A_surface)/(rho*V*c)*(T - Tinf) 
+
+   
+
+class node:
+    def __init__(n, T, rho, V, c, q=0, E_g=0):
+        n.rho = rho
+        n.T = T
+        n.V = V
+        n.c = c 
+        n.q = q
+        n.E_g = E_g
+    def getT(n):
+        return n.T
+    def getArgs(n):
+        return [n.rho, n.V, n.c, n.q, n.E_g]
+
+
+class path: 
+    def __init__(p, A1, A2=0, Tinf = 298, h=0, e=0, k=0, L=0):
+        p.A1 = A1
+        if A2 == 0:
+            p.A2 = A1
+        p.h = h
+        p.e = e
+        p.k =k
+        p.L = L
+        p.Tinf = Tinf
+    def getArgs(p): 
+        return [p.Tinf, p.h, p.A1, p.A2, p.e, p.k, p.L]
+        
+
+def ODE(t, T, Tinf, h, A1, A2, e, k ,L, rho, V, c, q, E_g):
+
+    return q*A1 + E_g - (h*A1)/(rho*V*c)*(T - Tinf)
+
+# def ODE(t, T, path.getArgs()):
+#     return T.q*T.A + T.E_g - (path.h*T.A)/(T.rho*T.V*T.c)*(T.T- path.Tinf)
+
+
+    
+
 
     
