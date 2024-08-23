a414e245400470cc12188db81f3f5ac77ad6fe81
Aiden Ryan <aiden.ryan@entegris.com>
2024-08-23 16:34:28 -0700

    Thermal Solver is functional
    t

--- thermal_notebook.py
+++ thermal_notebook.py
@@ -3,11 +3,9 @@ import sys
 sys.path.append('/Users/125715/python')
 import math as m
 import numpy as np
-
 from fluidlib import thermal_solver as ts
-from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
 
 
 
@@ -18,19 +16,30 @@ Bi = h*dx/k
 Fo = alpha*t/dx**2
 '''
 n = 2
-t = 400
+t = 3600
 step = 1
 dt = int(t/step)
 T = np.zeros(shape=(n,dt))
-T[:,0] = np.random.random_integers(90,700, n)
+T[:,0] = [200,150]
 Tinf = 298
-dx = 10
+dx = 1
 h = 50
-k = 50
+k = 45
+A=.01
 print(ts.ThermalSolve(h, dx, k, 0.001, dt, T, Tinf))
 t = np.linspace(0,t,dt)
 for i in range(len(T[:,0])):
     plt.plot(t, T[i, :])
 
 
+# %%
+rho = 7980
+T1 = ts.node(300, 7980, .1)
+T2 = ts.node(100, rho, 0.1)
+P1 = ts.path(.01, k = 25, dx=1)
+
+T_array = ts.nodeArray()
+T_array.build_array(T1,T2)
+print(T_array.get_array())
+solution = ts.buildNetwork(T_array, P1)
 # %%
--- thermal_solver.py
+++ thermal_solver.py
@@ -5,43 +5,61 @@ constant heat transfer coefficient
 
 '''
 import numpy as np
-   
+import CoolProp.CoolProp as CP
 
 class node:
     def __init__(n, T, rho=0, V=0, c=0, q=0, E_g=0):
-        n.rho = rho
-        n.T = T
-        n.V = V
-        n.c = c 
-        n.q = q
-        n.E_g = E_g
+        #Build a Thermal node object that has attributes T, rho, V, C, q, and E_g
+        n.rho = rho #kg/m3
+        n.T = T #K
+        n.V = V #m3
+        n.c = c #J/kgK
+        n.q = q #W
+        n.E_g = E_g #W
     def getT(n):
         return n.T
     def getArgs(n):
         return [n.rho, n.V, n.c, n.q, n.E_g]
-
+    
+class nodeArray:
+    def __init__(self) -> None:
+        self.array = None
+    def build_array(self, *nodes):
+        params_list = [node.T for node in nodes]
+        self.array = np.array(params_list)
+    def get_array(self):
+        return self.array
 
 class path: 
-    def __init__(p, A1, A2=0, Tinf = 298, h=0, e=0, k=0, L=0):
+    def __init__(p, A1, Tinf = 298, h=0, e=0, k=0, dx=0):
+        #Builds a thermal path object that has attributes A, Tinf, h, e, k, and dx
         p.A1 = A1
-        if A2 == 0:
-            p.A2 = A1
         p.h = h
         p.e = e
         p.k =k
-        p.L = L
+        p.dx = dx
         p.Tinf = Tinf
     def getArgs(p): 
-        return [p.Tinf, p.h, p.A1, p.A2, p.e, p.k, p.L]
+        return [p.Tinf, p.h, p.A1, p.e, p.k, p.dx]
         
+class buildNetwork(node, path):
+    
+    def sol(nodeArray, path):
+        pathArgs = path.getArgs(node)
+        return[
+            '''-k*A*dT/dx'''
+            -pathArgs[4]*pathArgs[2]*nodeArray[0]-nodeArray[1]/pathArgs[6]
+        ]
+
+
+
 
 def ThermalSolve(h,dx,k,alpha,t,T,Tinf=298):
-
     Bi = h*dx/k
     Fo = alpha*t/dx**2
     if Fo*(1 + Bi) > .5: 
-          print(Fo*(1 + Bi)) 
-          return("Does not meet stability criterion") 
+        print(Fo*(1 + Bi)) 
+        return("Does not meet stability criterion") 
 
     jsize = len(T[:,0]-1)
     for i in range(t-1):
