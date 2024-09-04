38c4dc5361c0304bfe8b3595c5e4ff0c970a1405
Aiden Ryan <aiden.mc.ryan@gmail.com>
2024-09-04 12:42:10 -0700

    Commit 9-4-2024

---
+++ mat_lib.py
@@ -0,0 +1,33 @@
+import CoolProp.CoolProp as CP
+
+class Material:
+    def __init__(self,density, TS = 0, YS = 0, BS = 0,  E=0, Charpy =0, Izod =0, 
+                 resitivity=0, magneticPermeability =0, poissonsRatio =0,
+                 specificHeatCapacity=0, k = 0, meanMeltingPoint = 0, CTE20C =0, CTE250C = 0, CTE500C = 0,shearStrength =0):
+        self.density = density #kg/m3
+        self.TS = TS #MPa
+        self.YS = YS #MPa
+        self.BS = BS #MPa
+        self.E = E #GPa
+        self.Charpy = Charpy #J
+        self.Izod = Izod #J
+        self.resistivity = resitivity #ohm-cm
+        self.magneticPermeability = magneticPermeability
+        self.poissonsRatio = poissonsRatio
+        self.shearStrength = shearStrength
+        self.specificHeatCapacity = specificHeatCapacity #J/kg-K
+        self.k = k #W/m-K
+        self.meanMeltingPoint = meanMeltingPoint #K
+        self.CTE20C = CTE20C #micrometers/m-C
+        self.CTE250C = CTE250C
+        self.CTE500C = CTE500C
+
+
+'''MATERIAL PROPERTIES SOURCED FROM MATWEB'''
+SS316 = Material(density=800, TS=580, YS=290, E=193, Charpy=105, Izod=129, resitivity=7.4e-5, magneticPermeability=1.008, specificHeatCapacity=500, k = 16.3, CTE20C = 16, CTE250C = 16.2, CTE500C = 17.5, meanMeltingPoint=1385)
+Al6061 = Material(density=270, TS=310, YS=276, BS=607, E=68.9, poissonsRatio=0.33, shearStrength= 207, resitivity=3.99e-6, specificHeatCapacity=896, k = 167, meanMeltingPoint=617)
+
+def getMatProp(Material,prop):
+    return getattr(Material, prop)
+
+
--- thermal_solver.py
+++ thermal_solver.py
@@ -7,38 +7,42 @@ ignore radiation
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-# import prop_lib as p
+import mat_lib as p
 
 
+material_mapping = {
+    'SS316': "p.SS316",
+    "SS316": "p.SS316",
 
+}
 class Node:
     '''
     Creates Temperature node with HT attributes
     '''
-    def __init__(self, T, kcond, Ac=0, Ar = 0,rho = 803, V=.01, c=1, q=0, Eg=0, h=0,Tinf=298,Tsurr=298,e = 0, mat='N/A'):
+    # def __init__(self, T, kcond, Ac=0, Ar = 0,density = 803, V=.01, c=1, q=0, Eg=0, h=0,Tinf=298,Tsurr=298,e = 0, mat = 'SS316'):
+    def __init__(self, T, mat=p.SS316,  Ac=0, Ar=0, V= 0.01, q=0, Eg = 0, e = 0,h= 0,Tinf=298,Tsurr=298): 
 
-        if (h*V/(Ac*kcond) > 0.1 ):
-            raise Exception("Biot Number is greater than 0.1, does not satisfy Lumped Capacitance Criterion.")
+
+        #316 is default material
+        self.mat = eval(material_mapping.get(mat))
+        self.V = V #m3
+        self.T = T #K 
+        self.q = q #W
+        self.Eg = Eg #W
+        self.h = h #W/m^2K
+        self.Tinf = Tinf #K
+        self.Tsurr = Tsurr #K
+        self.e = e # dimensionless, emissivity of body
+        self.Ac = Ac #Convection Area
+        self.Ar = Ar #Radiation Area
+        self.density = p.getMatProp(self.mat, "density")#kg/m3 
+        self.k= p.getMatProp(self.mat, "k") #W/m-K
+        self.c = p.getMatProp(self.mat, "specificHeatCapacity") #J/kgK
+        if (self.h*self.V/(Ac*self.k) > 0.1 ):
+                raise Exception("Biot Number is greater than 0.1, does not satisfy Lumped Capacitance Criterion.")
         
-        else:
-            #316 is default material
-            # self.rho = p.SS316.density #kg/m3 
-            self.rho = rho #kg/m3 
-            self.V = V #m3
-            self.T = T #K 
-            self.c = c #J/kgK
-            self.q = q #W
-            self.Eg = Eg #W
-            self.h = h #W/m^2K
-            self.Tinf = Tinf #K
-            self.Tsurr = Tsurr #K
-            self.e = e # dimensionless, emissivity of body
-            self.Ac = Ac #Convection Area
-            self.Ar = Ar #Radiation Area
-            self.mat = mat
-            self.kcond = kcond
     def get_args(self):
-        return [self.rho, self.c, self.q, self.Eg, self.h, self.Tinf, self.Tsurr, self.e,self.Ac,self.Ar,self.V]
+        return [self.density, self.c, self.q, self.Eg, self.h, self.Tinf, self.Tsurr, self.e,self.Ac,self.Ar,self.V]
     
 class Path:
     '''
@@ -57,7 +61,7 @@ def TMatrix(nodes):
     Creates Temperature attribute matrix to be passed into T_vs_t function
     '''
     array1 = np.array([node.T for node in nodes], dtype= float)
-    array2 = np.array([node.rho for node in nodes],dtype= float)
+    array2 = np.array([node.density for node in nodes],dtype= float)
     array3 = np.array([node.c for node in nodes],dtype= float)
     array4 = np.array([node.q for node in nodes],dtype= float)
     array5 = np.array([node.Eg for node in nodes],dtype= float)
@@ -68,7 +72,7 @@ def TMatrix(nodes):
     array10 = np.array([node.Ac for node in nodes],dtype= float)
     array11 = np.array([node.Ar for node in nodes],dtype= float)
     array12 = np.array([node.V for node in nodes],dtype= float)
-    array13 = np.array([node.kcond for node in nodes], dtype=float)
+    array13 = np.array([node.k for node in nodes], dtype=float)
     return np.vstack((array1, array2, array3, array4, array5,array6, array7, array8, array9,array10,array11, array12,array13))
 
 def pMatrix(paths, n):
@@ -116,7 +120,6 @@ def T_vs_t(t_span, t_eval, nodeMatrix, pathMatrix):
         A = pathMatrix[0,:]
         k = pathMatrix[1,:]
         dx = pathMatrix[2,:]
-   # Fo = kcond*t_eval[1]/(rho*c*dx**2)
 
     dTdt = np.zeros(n,dtype=float)
     def func(t, T_array,rho,c,q,Eg,Tinf,h,e,A,k,dx,Ac,Ar,V):
@@ -129,7 +132,7 @@ def T_vs_t(t_span, t_eval, nodeMatrix, pathMatrix):
             - h[1]*Ac[1]*(T_array[1] - Tinf[1]) - sig*e[1]*Ar[1]*(T_array[1]**4 - Tsurr[1]**4))/(rho[1] *c[1]*V[1])
             return dTdt       
         elif n == 1:
-            return - h*Ac*(T_array - Tinf) - sig*e*Ar*(T_array**4 - Tsurr**4)/(rho *c*V)
+            return q+Eg- h*Ac*(T_array - Tinf) - sig*e*Ar*(T_array**4 - Tsurr**4)/(rho *c*V)
         else:
             for i in range(n):
                 if i == 0:
@@ -176,24 +179,18 @@ def ThermalSolve():
         print("ENTER NODE ATTRIBUTES. \n")
         if n == 1:
                 T = float(input("ENTER TEMPERATURE OF LUMPED NODE: "))
-                kcond = float(input("ENTER THERMAL CONDUCTIVITY OF NODE: "))
+                mat = input("ENTER MATERIAL: ")
                 h = float(input("ENTER HEAT TRANSFER COEFFICIENT: " ))
                 V = float(input("ENTER VOLUME OF LUMPED NODE: "))
                 Ac = float(input("ENTER CONVECTIVE AREA OF LUMPED NODE: "))
-                print('\n')
-                if (h*V/(Ac*kcond) > 0.1 ):
-                    raise Exception("Biot Number is greater than 0.1, does not satisfy Lumped Capacitance Criterion.")
                 Ar = float(input("ENTER RADIATIVE AREA OF LUMPED NODE: "))
-                rho = float(input("ENTER DENSITY OF LUMPED NODE: "))
-                c = float(input("ENTER SPECIFIC HEAT CAPACITY OF LUMPED NODE: "))
                 q = float(input("ENTER HEATFLUX THROUGH LUMPED NODE: "))
                 Eg = float(input("ENTER HEAT GENERATED FROM LUMPED NODE: "))
                 Tinf = float(input("ENTER TEMPERATURE OF FREESTREAM: "))
                 Tsurr = float(input("ENTER TEMPERATURE OF SURROUNDINGS: "))
                 e = float(input("ENTER EMISSIVITY OF LUMPED NODE: "))
-                mat = input("ENTER MATERIAL: ")
-                print('\n')
-                nodes[0] = Node(T,kcond, Ac, Ar, rho, V,c, q, Eg, h, Tinf, Tsurr, e, mat)
+                nodes[0] = Node(T,mat, Ac, Ar, V, q, Eg, e, h , Tinf, Tsurr)
+                paths = [0,0,0]
         else:
             for i in range(n):
                 T = float(input("ENTER TEMPERATURE OF LUMPED NODE " +str(i+1)+": "))
@@ -216,16 +213,15 @@ def ThermalSolve():
                 nodes[i] = Node(T,kcond, Ac, Ar, rho, V, c, q, Eg, h, Tinf, Tsurr, e, mat)
         
         if n == 2:
-                print("ENTER PATH ATTRIBUTES. ENTER 0 TO IGNORE PARAMETER. \n")
+                print("ENTER PATH ATTRIBUTES. ENTER 0 TO IGNORE PARAMETER. ")
                 A = float(input("ENTER CONDUCTION PATH AREA: "))
                 k = float(input("ENTER CONDUCTION COEFFICIENT: "))
                 dx = float(input("ENTER DISTANCE BETWEEN NODES: "))
                 print('\n')
                 paths= Path(A, k, dx)
-        else:
-            print("ENTER PATH ATTRIBUTES. ENTER 0 TO IGNORE PARAMETER. \n")
+        elif n > 2:
+            print("ENTER PATH ATTRIBUTES. ENTER 0 TO IGNORE PARAMETER. ")
             for i in range(n-1):
-            
                 A = float(input("ENTER CONDUCTION PATH AREA: "))
                 k = float(input("ENTER CONDUCTION COEFFICIENT: "))
                 dx = float(input("ENTER DISTANCE BETWEEN NODES: "))
@@ -233,19 +229,22 @@ def ThermalSolve():
                 print('\n')
 
         #sets up nodes for Tvst function
-        nodeMatrix = TMatrix(nodes)
-        pathMatrix = pMatrix(paths, n-1)
-
+        if n >1:
+            nodeMatrix = TMatrix(nodes)
+            pathMatrix = pMatrix(paths, n-1)
+        else:
+            nodeMatrix = TMatrix(nodes)
+            pathMatrix = paths
         '''
         Check if nodes satisfy Lumped Capacitance and Convergence Criterion
         Biot = hL/k
         Fo = k*dt/(rho*cp*dx**2)
         ''' 
         #Set time condition
-        t0 = float(input("ENTER TIME START: "))
-        tf = float(input("ENTER TIME STOP: "))
+        t0 = int(input("ENTER TIME START: "))
+        tf = int(input("ENTER TIME STOP: "))
         t = [t0,tf]
-        t_eval = np.linspace(t[0], t[1], tf-t0)
+        t_eval = np.linspace(t[0], t[1], (tf-t0)*1000)
     
     #Solve
     sol = T_vs_t(t, t_eval = t_eval,nodeMatrix=nodeMatrix, pathMatrix=pathMatrix)
@@ -260,11 +259,11 @@ def ThermalSolve():
     plt.show()
     y = input("Would you like to change your time window? Y/N: ")
     if y == "Y" or y == "y":
-        while y != "N" or y != "n": 
-            t0 = float(input("ENTER TIME START: "))
-            tf = float(input("ENTER TIME STOP: "))
+        while y != "N": 
+            t0 = int(input("ENTER TIME START: "))
+            tf = int(input("ENTER TIME STOP: "))
             t = [t0,tf]
-            t_eval = np.linspace(t[0], t[1], tf - t0)
+            t_eval = np.linspace(t[0], t[1], int((tf-t0)/.001))
             sol = T_vs_t(t, t_eval = t_eval,nodeMatrix=nodeMatrix, pathMatrix=pathMatrix)
             for i in range(n):
                 plt.plot(sol.t, sol.y[i],label=str(sol.y[i,0]))
@@ -274,3 +273,4 @@ def ThermalSolve():
                 plt.legend()
             plt.show()
             y = input("Would you like to change your time window? Y/N: ")
+        print("goodbye.")
