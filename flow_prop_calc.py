8c2569f7037fe0601a3f56b813fe226bced0382e
Aiden Ryan <aiden.mc.ryan@gmail.com>
2024-09-30 15:46:49 -0700

    Rewrote thermal solver to calculate heat transfer between N nodes simultaneously (N-D vs 1-D)

--- flow_prop_calc.py
+++ flow_prop_calc.py
@@ -1,30 +1,40 @@
 import conversion_library as c 
-import numpy as np
 '''NUSSELT NUMBER CALCULATOR: SOLVE FOR NUSSELT NUMBER USING SIMPLIFIED GEOMETRIES'''
 
-def getRe(u, fluid, D, P2prop, P2, Lc):
-    mu = c.getFluidProperty(hOut = 'V', hIn='D' + P2prop, val1 = D, val2=P2, hFLd=fluid)
-    return D*u*Lc/mu
-
+def getRe(u, fluid, D, Pressure, Lc):
+    '''Returns Reynolds number, requires pressure and density input
+    Lc == 4*Ac/P'''
+    Viscosity = c.getFluidProperty(hOut = 'V', hIn='DP', val1 = D, val2=Pressure, hFLd=fluid)
+    return D*u*Lc/Viscosity
     
-
-    
-
 def flatPlateNu(Re, Pr,type='AVERAGE'):
+    '''Returns Nusselt Number for a flat plate for a provided Reynolds number and Prandtl number'''
     '''TYPE - LOCAL OR AVERAGE'''
     if Pr < 0.6: 
         raise Exception("Pr must be greater than or equal to 0.6")
-    
-    if Re < 2300:
+    if Re < 5e5:
         '''LAMINAR FLOW'''
         if type == 'LOCAL':
             return 0.332*Re**0.5*Pr**(1/3)
         elif type == 'AVERAGE':
             return 0.664*Re**0.5*Pr**(1/3)
-    elif Re > 2300 and Re < 4000:
+    elif Re == 5e5:
         '''TRANSIENT'''
-    elif Re > 4000:
+    elif Re > 5e5 and Re < 1e7:
         '''TURBULENT'''
-        if type == 'LOCAL':
+        if type == 'LOCAL' and (Pr > 0.6 and Pr < 60):
             return 0.0296*Re**(4/5)*Pr**(1/3)
         
+def internalFlowNu(Re, Pr=0.6, BoundaryCondition = 'constantQ', f = 1):
+    '''Returns Nusselt Number for circular tube: 
+    Boundary Conditions:
+    constantQ;
+    constantTs'''
+    if Re < 2300 and BoundaryCondition == 'constantQ':
+        '''LAMINAR FLOW fully developed and constant surface heat flux'''
+        return 4.36
+    elif Re < 2300 and BoundaryCondition == 'constantTs': 
+        return 3.66
+    elif Re > 10^4 and Re < 5e6 and Pr > 0.5 and Pr < 2000:
+        '''Applies to constantQ and constantTs'''
+        return (f/8)*(Re*Pr)/(1.07 + 12.7*(f/8)**0.5*(Pr**(2/3)-1))
--- thermal_solver.py
+++ thermal_solver.py
@@ -19,47 +19,113 @@ class Node:
     '''
     Creates Temperature node with HT attributes
     '''
-    def __init__(self, T, medium, medium_type, V= 0.01, q_flux=0.0, Eg = 0.0, P = 0,isothermal = 0): 
+    def __init__(self, order, T, medium, medium_type, V= 0.01, Eg = 0.0, Pressure = 0,isothermal = 0,connectedPaths = None, Tsurr = 298.15, e = 0.0,Arad = 0.0): 
         '''
-        medium_type = FLUID or SOLID
+        T = type::float; initial temperature, K
+        
+        medium_type = "FLUID" or "SOLID"
+        
+        order attribute assigns sequence position
         '''
         if medium_type == "FLUID":
-            self.density = c.getFluidProperty('D', 'TP', T, P, medium) #kg/m3 
-            self.k = c.getFluidProperty('CONDUCTIVITY', 'TP', T, P, medium) #W/m-K
-            self.c = c.getFluidProperty('CPMASS', 'TP', T, P, medium) #J/kgK
+            self.density = c.getFluidProperty('D', 'TP', T, Pressure, medium) #kg/m3 
+            self.k = c.getFluidProperty('CONDUCTIVITY', 'TP', T, Pressure, medium) #W/m-K
+            self.c = c.getFluidProperty('CPMASS', 'TP', T, Pressure, medium) #J/kgK
         elif medium_type == "SOLID": 
             self.medium = eval(material_mapping.get(medium)) # type: ignore
             self.density = p.getMatProp(self.medium, "density") #kg/m3 
             self.k= p.getMatProp(self.medium, "k") #W/m-K
             self.c = p.getMatProp(self.medium, "specificHeatCapacity") #J/kgK
-        self.isothermal = isothermal
+        self.isothermal = isothermal #isothermal condition is true if isothermal == 1 
         self.V = V #m3
         self.T = T #K 
-        self.q_flux = q_flux #W/m^2
-        self.Eg = Eg #W
-        
-    def get_args(self):
-        return [self.T, self.density, self.c, self.q_flux, self.Eg, self.V]
+        self.Eg = Eg #W, heat generated
+        self.order = order # type- int. dictates iteration order  
+        self.Tsurr = Tsurr #K
+        self.e = e #emissivity
+        self.Arad = Arad #m^2
+        if connectedPaths is None:
+            self.connectedPaths = [] #specifies which paths are connected to node
     
 class Path:
     '''
     Creates Conduction, Convection, or Radiation Pathway between Nodes
     Assumptions: Path is of Constant Area
     '''
-    def __init__(self, Acond=0.0, Aconv = 0.0, Arad = 0.0, k=0.0, dx=1.0, h=0, e=0.0, Tinf = 298.15, Tsurr = 298.15):
+    def __init__(self, nodeA, nodeB, Acond=0.0, Aconv = 0.0, Arad = 0.0, dx=1.0, h=0, e=0.0, Tinf = 298.15, Tsurr = 298.15):
         self.Acond = Acond #Conduction Area [m^2]
         self.Aconv = Aconv #Convection Area [m^2]
         self.Arad = Arad #Radiation Area [m^2]
-        self.k = k #conduction coefficient [W/mK]
         self.dx = dx #length [m]
         self.h = h #Overall heat transfer coefficient [W/m2K]
-        self.Tinf = Tinf #K
-        self.Tsurr = Tsurr #K
         self.e = e # dimensionless, emissivity of body's surface
-    def get_args(self):
-        return [self.Acond, self.Aconv, self.Arad, self.k, self.dx, self.h, self.e, self.Tinf,self.Tsurr]
+        self.nodeA = nodeA 
+        self.nodeB = nodeB
+        self.k = (nodeA.k+nodeB.k)/2  #conduction coefficient [W/mK]
 
-def nodeMatrix(nodes):
+def T_vs_t2(t, t_eval, T, path, nodes):
+    '''PATHS MUST BE SEQUENCED ACCORDING TO NODE ORDER. EX: path1(node1, node2) NOT path1(node2,node1)'''
+    n = len(nodes)
+    if n > 2:
+        for i in range(n):
+            for j in range(len(path)):  
+                if path[j].nodeA == nodes[i] or path[j].nodeB == nodes[i]: #if either of path j's nodes are equal to current node, append path to connected attribute path
+                    nodes[i].connectedPaths.append(j)
+    def func2(t,T):
+        dTdt = np.zeros(n, dtype=float)
+        if n == 2: #Only two nodes
+            k = path.k
+            h = path.h
+            dx = path.dx
+
+            Acond = path.Acond
+            Aconv = path.Aconv
+            
+            Arad0 = path.nodeA.Arad
+            Arad1 = path.nodeB.Arad
+
+            e0 = path.nodeA.e
+            e1 = path.nodeB.e
+            
+            Tsurr0 = path.nodeA.Tsurr
+            Tsurr1 = path.nodeB.Tsurr
+            sig = 5.678e-8
+
+            dTdt[path.nodeA.order] =  - k * Acond*(T[path.nodeA.order] - T[path.nodeB.order]) / (dx*path.nodeA.density*path.nodeA.V*path.nodeA.c) - h * Aconv *(T[path.nodeA.order] - T[[path.nodeB.order]]) / (path.nodeA.density*path.nodeA.V*path.nodeA.c) - (sig * e0 * Arad0 * (T[path.nodeA.order]**4 - Tsurr0**4)) /(path.nodeA.density*path.nodeA.V*path.nodeA.c)
+
+            dTdt[path.nodeB.order] =  - k * Acond*(T[path.nodeB.order] - T[path.nodeA.order]) / (dx*path.nodeB.density*path.nodeA.V*path.nodeA.c) - h * Aconv *(T[path.nodeB.order] - T[path.nodeA.order]) / (path.nodeB.density*path.nodeB.V*path.nodeB.c) - (sig * e1 * Arad1 * (T[path.nodeB.order]**4 - Tsurr1**4)) /(path.nodeB.density*path.nodeB.V*path.nodeB.c)
+
+            return dTdt
+        else:
+            for i in range(n):
+                # for node i get connectedPaths
+                P = nodes[i].connectedPaths
+                for j in range(len(P)):
+                    #Iterate thru each connected to node i
+                    '''Conduction Terms'''
+                    k = path[P[j]].k
+                    dx = path[P[j]].dx
+                    Acond = path[P[j]].Acond
+                    '''Convection Terms'''
+                    h = path[P[j]].h
+                    Aconv = path[P[j]].Aconv
+                    '''Radiation Terms'''
+                    e = path[P[j]].nodeA.e
+                    Tsurr = path[P[j]].nodeA.Tsurr
+                    Arad = path[P[j]].nodeA.Arad
+                    sig = 5.678e-8
+                    #get temperature associated with path[p[j]] order must be maintained. 
+                    T1 = T[path[P[j]].nodeA.order] 
+                    T2 = T[path[P[j]].nodeB.order] 
+                    if path[j].nodeA.order == i: #If T being accessed is from iteration node; use this equation
+                        dTdt[i] = dTdt[i] - k*Acond*(T1-T2) / (dx*path[P[j]].nodeA.density*path[P[j]].nodeA.V*path[P[j]].nodeA.c) - h * Aconv * (T1 - T2) / (path[P[j]].nodeA.density*path[P[j]].nodeA.V * path[P[j]].nodeA.c) - e*sig*Arad*(T1**4 - Tsurr**4)/(path[P[j]].nodeA.density * path[P[j]].nodeA.V * path[P[j]].nodeA.c) if nodes[i].isothermal == 0 else 0
+                    elif path[j].nodeA.order != i: #If T being accessed is not from iteration node; use this equation
+                        dTdt[i] =  dTdt[i] + k*Acond*(T1-T2) / (dx*path[P[j]].nodeA.density*path[P[j]].nodeA.V*path[P[j]].nodeA.c) + h * Aconv * (T1 - T2) / (path[P[j]].nodeA.density*path[P[j]].nodeA.V * path[P[j]].nodeA.c) + e*sig*Arad*(T1**4 - Tsurr**4)/(path[P[j]].nodeA.density * path[P[j]].nodeA.V * path[P[j]].nodeA.c) if nodes[i].isothermal == 0 else 0
+            return dTdt 
+        
+    return solve_ivp(func2, t_span=t, t_eval=t_eval, y0=T,method='LSODA',atol=1e-7)
+
+# def nodeMatrix(nodes):
     '''
     Creates Node matrix to be passed into T_vs_t function
     '''
@@ -69,7 +135,7 @@ def nodeMatrix(nodes):
         attribute_arrays.append([getattr(node, attr) for node in nodes])
     return np.vstack(attribute_arrays)
 
-def pathMatrix(paths): 
+# def pathMatrix(paths): 
     '''
     Creates path attribute matrix to be passed into T_vs_t function
     '''
@@ -79,7 +145,7 @@ def pathMatrix(paths):
         attribute_arrays.append([getattr(path, attr) for path in paths])
     return np.vstack(attribute_arrays)
  
-def T_vs_t(t_span, t_eval, nodeMatrix, condPath=np.zeros(10), convPath=np.zeros(10), radPath=np.zeros(10)):
+# def T_vs_t(t_span, t_eval, nodeMatrix, condPath=np.zeros(10), convPath=np.zeros(10), radPath=np.zeros(10)):
     '''
     Iteratively solves Thermal Network. Returns Temperature vs time plot.
     '''
@@ -136,12 +202,12 @@ def T_vs_t(t_span, t_eval, nodeMatrix, condPath=np.zeros(10), convPath=np.zeros(
         e = radPath[6,:]
         Tsurr = radPath[8,:]
 
-    '''Biot Check'''
+    '''BIOT CHECK'''
     for i in range(n-1):
         a = h[i]*V[i]/(Aconv[i]*kcheck[i])
         if a > 0.1:
             raise Exception("BIOT NUMBER GREATER THAN 0.1, DOES NOT SATISFY LUMPED CAPACITANCE CRITERIA")
-    dTdt = np.zeros(n,dtype=float)
+    dTdt = np.zeros(n, dtype=float)
 
     def func(t, T,rho,c,q_flux,Eg,Tinf,h,e,k,dx,Aconv,V,Acond=np.zeros(n),Arad = np.zeros(n),isoT=np.zeros(n)):
         sig = 5.67e-8 #Stefan-Boltzmann Constant [W/m^2/K^4]
@@ -165,10 +231,12 @@ def T_vs_t(t_span, t_eval, nodeMatrix, condPath=np.zeros(10), convPath=np.zeros(
                 else:
                     dTdt[i] = q_flux[i] + Eg[i] + (-k[i-1]*Acond[i-1]*(T[i] - T[i-1]) / (rho[i] * c[i] * dx[i-1] * V[i])
                             - k[i]*Acond[i]*(T[i] - T[i+1]) / (rho[i] * c[i] * dx[i] * V[i]))+(-h[i]*Aconv[i]*(T[i] - Tinf[i])- sig*e[i]*Arad[i]*(T[i]**4 - Tsurr[i]**4))/(rho[i] * c[i]*V[i]) if isoT[i] == 0 else 0
-            return dTdt
-        
+            return dTdt   
     return solve_ivp(func, t_span=t_span, t_eval=t_eval, y0=T, args=(rho,c,q_flux,Eg,Tinf,h,e,k,dx,Aconv,V,Acond,Arad,isoT))
-    
+
+
+# return solve_ivp(func2, t_span = t_span, t_eval=t_eval, y0=T, args=()  )
+
 # def ThermalSolve():
     print("--------------------------\n1-D TRANSIENT THERMAL SOLVER\nBy: Aiden Ryan\nDescription: \nSolver uses lumped capacitance model and scipy's solve_ivp package to output Acond Temperature vs Time Plot \nof N number of thermal nodes. \n---\nREQUIRED PACKAGES: \n-numpy\n-scipy\n-matplotlib ")
     print("---\nOTHER REQUIREMENTS: \nEnsure Biot Number for each node is < 0.1 \nBi = h*L_c/k \n")
